1a9cc5c1c3cddb65ce3fec6cadfd06057310c0d2
biodunseyi <adebayoabiodun31122004@gmail.com>
2023-07-05 14:10:24 +0100

    Worked on digit data type

---
+++ datatypes/digit.py
@@ -0,0 +1,62 @@
+# This is a single line comments
+"""
+This is a multi-line comments
+hello PC
+"""
+
+# Variable Names
+num1 = 200
+num2 = 300
+num3 = 0.5
+num4 = 0.5
+num5 = 3
+num6 = 2
+num7 = 400
+# Addition and subtraction
+# print(type(num1))
+answer = num4 + num3
+
+# print(type(answer), answer)
+# type(variable) to get the type of data value for a variable
+
+# multiplication, exponents and roots
+
+# '*' means multiplication
+
+mult_answer = num2 * num3
+
+#print(type(mult_answer), mult_answer)
+
+# "**" means exponents
+square_answer = num1**2
+
+# print(type(square_answer), square_answer)
+
+cube_answer = num5**3
+
+# print(type(cube_answer), cube_answer)
+
+quad_answer = num6**4
+# print(type(quad_answer), quad_answer)
+
+root_answer = num7**0.5
+
+# print(type(root_answer), root_answer)
+
+# division, remainder, root division
+
+var1 = 5
+var2 = 2
+
+# normal division Output always float
+divided_answer = var1 / var2
+print(type(divided_answer), divided_answer)
+
+# Root division Output always int
+root_div_ans = var1 // var2
+
+print(type(root_div_ans), root_div_ans)
+
+modulo_ans = var1 % var2
+
+print(type(modulo_ans), modulo_ans)
