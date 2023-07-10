6b0be62665daae78fe942fba92987442f0bb4dc7
biodunseyi <adebayoabiodun31122004@gmail.com>
2023-07-10 11:41:56 +0100

    string and string operation

---
+++ datatypes/stringintro.py
@@ -0,0 +1,51 @@
+"""
+  Introduction to data types
+  * strings in python
+  * String Manipulation
+"""
+
+name = "Bello"
+surname = 'Bayo'
+
+# print(name, surname)
+
+
+vegetables = "carrot"
+sauce = "Klc"
+
+# print(sauce, vegetables)
+# string concatenation. Addition
+food = vegetables + " " + sauce
+
+print(food)
+
+print(surname + " " + name)
+
+
+print((name + " ") * 3)
+
+"""
+  Introduction to data types
+  * strings in python
+  * String Manipulation
+"""
+
+# in operator
+name = "o"
+second = "a"
+
+number1 = "23"
+
+number2 = "13"
+
+print(type(name))
+
+answer = int(number1) * int(number2)
+print(answer)
+print(type(answer))
+
+# non-numeric number can't be converted to string
+# result = int(name) + int(second)
+# print(result)
+# print(type(result))
+
---
+++ datatypes/stroperation.py
@@ -0,0 +1,46 @@
+"""
+  Introduction to data types
+  * strings in python
+  * String Manipulation
+"""
+
+# in operator
+sentence1 = "Hi, My name is Bayo."
+
+"""
+  in operators print True/False only
+  It is case sensitive
+"""
+
+print("Bayo" in sentence1)
+print("seyi" in sentence1)
+print("namE" in sentence1)
+
+"""
+  Introduction to data types
+  * strings in python
+  * String Manipulation
+"""
+
+# ord, len, str, chr built in methods for string operation
+
+name = 'Bello AHADVDBAJNAS CDBDNFDNFV IVNJDIDFC JKVDKVDM '
+firstname = "Bayo"
+
+number1 = 30
+number2 = 22
+print(type(number1))
+
+new_num = str(number1)
+print(type(new_num))
+
+print(str(number1) + str(number2))
+
+# print(ord("a"))
+# print(ord("A"))
+# print(ord(" "))
+
+
+# print(chr(3397))
+
+# print(len(name))
