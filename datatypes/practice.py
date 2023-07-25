1813f20b38b29bec1f9ec0a694d6d18c844ccee4
biodunseyi <adebayoabiodun31122004@gmail.com>
2023-07-25 18:27:27 +0100

    practice

---
+++ datatypes/practice.py
@@ -0,0 +1,8 @@
+print("You are welcome to the basic operators of the following arithmetic operations: addition, subtraction, multiplication, modulo, and division")
+
+Value1 = int(input("What is the first value you would like to operate on? "))
+value2 = int(input("What is the second value you would like to operate on? "))
+#addition operation
+answer = Value1 + value2
+
+print(f"The answer of {Value1} + {value2} is = {answer}")
